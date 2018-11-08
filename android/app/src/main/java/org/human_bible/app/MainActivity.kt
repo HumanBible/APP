750ed13d527dab14aa1014dbe5cea6e66aed69ce
Etby Gao <etby.org@gmail.com>
2018-11-08 15:44:09 +0800

    init android project

---
+++ android/app/src/main/java/org/human_bible/app/MainActivity.kt
@@ -0,0 +1,19 @@
+package org.human_bible.app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import org.human_bible.app.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ android/app/src/main/java/org/human_bible/app/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package org.human_bible.app.ui.main
+
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
