fccbcabae8e259976cfe64b7df78d3be03795b65
Etby Gao <etby.org@gmail.com>
2018-11-08 15:52:38 +0800

    init ios project

---
+++ ios/app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app
+//
+//  Created by Etby Gao on 2018/11/8.
+//  Copyright © 2018 Human Bible. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
